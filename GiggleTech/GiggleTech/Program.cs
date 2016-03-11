589d852a4fec208649fa5dbd6b72c77bf9e2d709
Kieran Nelson <kierantnelson@gmail.com>
2016-03-11 14:30:56 +0000

    Random Numbers

--- GiggleTech/GiggleTech/Program.cs
+++ GiggleTech/GiggleTech/Program.cs
@@ -10,6 +10,12 @@ namespace GiggleTech
     {
         static void Main(string[] args)
         {
+            int length = 32;
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Random Number " + i + " : " + Convert.ToBase64String(Random.GenerateRAndomNumber(length)));
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ GiggleTech/GiggleTech/Random.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiggleTech
+{
+    class Random
+    {
+        public static byte[] GenerateRAndomNumber(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+    }
+}
